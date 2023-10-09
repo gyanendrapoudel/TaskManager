d6920574215ba4a9d1c81a46c26054de2d6c0bef
Gyanendra <gyanen56@gmail.com>
2023-10-09 16:26:26 +1100

    getting single  task logic

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,11 +4,22 @@ const getAllTasks = async (req,res)=>{
      const tasks = await Task.find({})
      res.status(200).json({tasks})
   } catch (error) {
+        res.status(500).json({ msg: error })
     
   }
 }
-const getTask = (req, res) => {
-  res.send('Get single Task')
+const getTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOne({_id:taskID})
+        if(!task){
+            return res.status(404).json({msg:`No task with id: ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+
 }
 
 const createTask = async (req, res) => {
